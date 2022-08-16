582a8ad4a9efe94cf3fa0473129998cdda57bf57
Mariusz <mariuszbia@yahoo.com>
2022-08-16 16:56:53 -0500

    Completed assignment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,35 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/todos" do
+    Todo.all.to_json(include: :category)
   end
 
+  post "/todos" do
+    todo = Todo.create(
+      category_id: params[:category_id],
+      title: params[:title],
+      description: params[:description],
+      due_date: params[:due_date],
+      effort: params[:effort]
+    )
+    todo.to_json
+  end
+
+  patch "/todos/:id" do
+    todo = Todo.find(params[:id])
+    todo.update(
+      title: params[:title],
+      description: params[:description],
+      due_date: params[:due_date],
+      effort: params[:effort]
+    )
+    todo.to_json
+  end
+
+  delete "/todos/:id" do
+    todo = Todo.find(params[:id])
+    todo.destroy
+    todo.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,37 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Category.create(name: "Cleaning")
+Category.create(name: "House Repairs")
+Category.create(name: "Car Maintenance")
+
+Todo.create(
+    category_id: 1,
+    title: "Weekly cleaning",
+    description: "Remember to clean behind the stove this time.",
+    due_date: "2022.08.21",
+    effort: 3
+)
+Todo.create(
+    category_id: 1,
+    title: "Window cleaning",
+    description: "It is this time of the year again. Do it before it gets too cold.",
+    due_date: "2022.08.31",
+    effort: 5
+)
+Todo.create(
+    category_id: 2,
+    title: "Fix garage door",
+    description: "Rails need to be aligned.",
+    due_date: "2022.12.21",
+    effort: 6
+)
+Todo.create(
+    category_id: 3,
+    title: "Fix headlight",
+    description: "Left headlight is out. Figure out if LED lightbulb would work and if rewiring is needed.",
+    due_date: "2022.09.30",
+    effort: 7
+)
 
 puts "✅ Done seeding!"
